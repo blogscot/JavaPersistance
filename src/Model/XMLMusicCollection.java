86bcdc54922efc7d906cb49471b1c34b6e855176
blogscot <iain.diamond@gmail.com>
2015-03-31 19:56:12 +0100

    Implemented XML loading using XPath

--- src/Model/XMLMusicCollection.java
+++ src/Model/XMLMusicCollection.java
@@ -3,14 +3,12 @@ package Model;
 import java.util.ArrayList;
 
 import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement(namespace="jaxb-xml-model")
+@XmlRootElement
 public class XMLMusicCollection {
 
-  @XmlElementWrapper(name = "musicList")
-  @XmlElement(name = "musicItem")
+  @XmlElement(name = "musicitem")
   private ArrayList<MusicItem> musicList;
 
   public void setMusicList(ArrayList<MusicItem> musicList) {
--- src/Model/XMLStorage.java
+++ src/Model/XMLStorage.java
@@ -7,11 +7,22 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 public class XMLStorage extends Storable {
 
-  @Override
-  public void load(File filename) throws PersistenceException {
+  public void loadWithJaxB(File filename) throws PersistenceException {
 
     JAXBContext context;
     try {
@@ -25,6 +36,56 @@ public class XMLStorage extends Storable {
       musicCollectionLength = musicList.size();
       
     } catch (JAXBException | IOException e) {
+      throw new PersistenceException();
+    }
+  }
+  
+  @Override
+  public void load(File filename) throws PersistenceException {
+    
+    // Clear out any old stuff first
+    musicList.clear();
+    
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setNamespaceAware(true);
+    DocumentBuilder builder;
+    XPath xpath = null;
+    Document doc = null;
+    
+    try {
+      builder = factory.newDocumentBuilder();
+      doc = builder.parse(filename);
+
+      // Create XPathFactory object
+      XPathFactory xpathFactory = XPathFactory.newInstance();
+
+      // Create XPath object
+      xpath = xpathFactory.newXPath();
+
+      // create XPathExpression object
+      XPathExpression expr = xpath.compile("/xmlMusicCollection/musicitem/artist/text()");
+      XPathExpression expr2 = xpath.compile("/xmlMusicCollection/musicitem/album/text()");
+      XPathExpression expr3 = xpath.compile("/xmlMusicCollection/musicitem/year/text()");
+      XPathExpression expr4 = xpath.compile("/xmlMusicCollection/musicitem/genre/text()");
+
+      // evaluate expressions result on XML document
+      NodeList nodes = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+      NodeList nodes2 = (NodeList) expr2.evaluate(doc, XPathConstants.NODESET);
+      NodeList nodes3 = (NodeList) expr3.evaluate(doc, XPathConstants.NODESET);
+      NodeList nodes4 = (NodeList) expr4.evaluate(doc, XPathConstants.NODESET);
+
+      for (int i = 0; i < nodes.getLength(); i++) {
+        MusicItem item = new MusicItem();
+        item.setArtist(nodes.item(i).getNodeValue());
+        item.setAlbum(nodes2.item(i).getNodeValue());
+        item.setYear(Integer.parseInt(nodes3.item(i).getNodeValue()));
+        item.setGenre(nodes4.item(i).getNodeValue());
+        musicList.add(item);
+      }
+      musicCollectionLength = musicList.size();
+      
+    } catch (ParserConfigurationException | SAXException | 
+        IOException |XPathExpressionException e) {
       throw new PersistenceException();
     }
   }
